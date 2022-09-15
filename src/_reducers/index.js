15bc3c53b9329a1b468f05edf5d7639c147e4b4f
Gyubin <gbcompany27@gmail.com>
2022-09-15 23:26:37 +0900

    add optionbutton and browse option button to browse page

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 
 import nav from "./nav";
+import browse from "./browse";
 
 const rootReducer = combineReducers({
-    nav
+    nav, browse
 });
 
 export default rootReducer;
--- src/components/views/Browse/Browse.js
+++ src/components/views/Browse/Browse.js
@@ -1,11 +1,26 @@
 import './Browse.css';
 
+import { useSelector,useDispatch } from "react-redux";
+import { changeBrowseOption } from '../../../_reducers/browse';
+
 import DesktopHeader from '../../library/ui/header/DesktopHeader/DesktopHeader';
+import OptionButton from '../../library/ui/unit/OptionButton/OptionButton';
 
 export default function Browse() {
+    const {browseOption} = useSelector(state => state.browse);
+    const dispatch = useDispatch();
+    const browseOptionChangeEvent = (option) => {
+        dispatch(changeBrowseOption(option));
+    }
+
     return (
         <div className='page'>
             <DesktopHeader></DesktopHeader>
+            <div className='browse-container'>
+                <div className='browse-option-button-container'>
+                    <OptionButton state={browseOption} option1={'인기'} option2={'최신'} clickEvent={browseOptionChangeEvent}></OptionButton>
+                </div>
+            </div>
         </div>
     )
 }
