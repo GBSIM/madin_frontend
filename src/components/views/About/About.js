e52d0bed3514c21c1733f8dfb586e6cdff2d13ef
Gyubin <gbcompany27@gmail.com>
2022-09-20 20:23:27 +0900

    add mobile about image

--- src/components/views/About/About.js
+++ src/components/views/About/About.js
@@ -8,6 +8,7 @@ export default function About() {
             <DesktopHeader></DesktopHeader>
             <div className='about-image-container'>
                 <img src={require('../../library/images/madin_about.png')} className='about-image'></img>
+                <img src={require('../../library/images/madin_about_mobile.png')} className='about-image-mobile'></img>
             </div>
             <div className='about-information-container'>
                 <div className='about-information'>
