1e1b59a4a3f3768337ccdb57d85af5e9bd1694bf
Gyubin <gbcompany27@gmail.com>
2022-09-20 20:35:19 +0900

    add logo button to mobile header

--- src/components/library/ui/header/MobileHeader/MobileHeader.js
+++ src/components/library/ui/header/MobileHeader/MobileHeader.js
@@ -1,9 +1,13 @@
 import './MobileHeader.css';
 
+import LogoButton from '../../logo/LogoButton/LogoButton';
+
 export default function MobileHeader() {
     return (
         <div className="mobile-header-container">
-           
+           <div className='mobile-header-logo-container'>
+                <LogoButton width={'70px'}></LogoButton>
+            </div>
         </div>
     )
 }
