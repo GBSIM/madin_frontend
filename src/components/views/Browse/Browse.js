ebbb4936ad0d3ca4d1efd1151942608626f15f6b
gyubin <gbcompany27@gmail.com>
2022-09-24 13:41:16 +0900

    browse page code refactoring

--- src/components/views/Browse/Browse.js
+++ src/components/views/Browse/Browse.js
@@ -1,28 +1,14 @@
 import './Browse.css';
 
-import { useSelector,useDispatch } from "react-redux";
-import { changeBrowseOption } from '../../../_reducers/browse';
-
 import Header from '../../library/ui/header/Header/Header';
 import Footer from '../../library/ui/footer/Footer/Footer';
-import TwoOptionsButton from '../../library/ui/unit/TwoOptionsButton/TwoOptionsButton';
+import BrowseOptionButton from '../../library/browse/BrowseOptionButton/BrowseOptionButton';
 
 export default function Browse() {
-    const {browseOption} = useSelector(state => state.browse);
-    const dispatch = useDispatch();
-    const browseOptionChangeEvent = (option) => {
-        dispatch(changeBrowseOption(option));
-        window.scrollTo(0,0);
-    }
-
     return (
         <div className='page'>
             <Header></Header>
-            <div className='browse-container'>
-                <div className='browse-option-button-container'>
-                    <TwoOptionsButton state={browseOption} option1={'인기'} option2={'최신'} clickEvent={browseOptionChangeEvent}></TwoOptionsButton>
-                </div>
-            </div>
+            <BrowseOptionButton></BrowseOptionButton>
             <Footer></Footer>
         </div>
     )
