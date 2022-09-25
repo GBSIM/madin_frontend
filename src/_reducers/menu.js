635ef14d63887cea0d6f0587c3b7cd42b380b8a6
gyubin <gbcompany27@gmail.com>
2022-09-25 17:05:00 +0900

    implement menu change function between dessert and drink

--- src/_reducers/menu.js
+++ src/_reducers/menu.js
@@ -16,8 +16,9 @@ const initialState = {
     menuDescription2: "설명2",
     menuDescription3: "설명3",
     menuDescription4: "설명4",
-    currentMenuIndex: 0,
-    menuList: [
+    currentDessertIndex: 0,
+    currentDrinkIndex: 0,
+    dessertList: [
         {"koreanName":"레몬마들렌","englishName":"Lemon Madeliene",
          "description1":"상큼한 레몬과 마들렌의 조합","description2":"마딘의 시그니쳐 메뉴에요.",
          "description3":"마딘의 첫 오픈부터 지금까지","description4":"가장 많이 사랑받는 디저트입니다."},
@@ -27,59 +28,126 @@ const initialState = {
          {"koreanName":"애플시나몬마들렌","englishName":"Apple Cinnamon Madeliene",
          "description1":"달콤한 사과와 마들렌의 조합","description2":"마딘의 시그니쳐 메뉴에요.",
          "description3":"마딘의 첫 오픈부터 지금까지","description4":"가장 많이 사랑받는 디저트입니다."},
-    ]
+    ],
+    drinkList: [
+        {"koreanName":"아메리카노","englishName":"Americano",
+         "description1":"상큼한 레몬과 마들렌의 조합","description2":"마딘의 시그니쳐 메뉴에요.",
+         "description3":"마딘의 첫 오픈부터 지금까지","description4":"가장 많이 사랑받는 디저트입니다."},
+        {"koreanName":"카페라떼","englishName":"Cafe Latte",
+         "description1":"깊은 초코의 맛과 마들렌의 조합","description2":"마딘의 시그니쳐 메뉴에요.",
+         "description3":"마딘의 첫 오픈부터 지금까지","description4":"가장 많이 사랑받는 디저트입니다."},
+         {"koreanName":"말차 라떼","englishName":"Green Tea Latte",
+         "description1":"달콤한 사과와 마들렌의 조합","description2":"마딘의 시그니쳐 메뉴에요.",
+         "description3":"마딘의 첫 오픈부터 지금까지","description4":"가장 많이 사랑받는 디저트입니다."},
+    ],
 }
 
 const menu = (state = initialState, action) => {
     switch (action.type) {
         case CHANGE_MENU_TYPE:
-            return {
-                ...state,
-                menuType: action.menuType
+            if (action.menuType === '디저트') {
+                return {
+                    ...state,
+                    menuType: action.menuType,
+                    menuEnglishName: state.dessertList[state.currentDessertIndex]["englishName"],
+                    menuKoreanName: state.dessertList[state.currentDessertIndex]["koreanName"],
+                    menuDescription1: state.dessertList[state.currentDessertIndex]["description1"],
+                    menuDescription2: state.dessertList[state.currentDessertIndex]["description2"],
+                    menuDescription3: state.dessertList[state.currentDessertIndex]["description3"],
+                    menuDescription4: state.dessertList[state.currentDessertIndex]["description4"],
+                }
+            } else {
+                return {
+                    ...state,
+                    menuType: action.menuType,  
+                    menuEnglishName: state.drinkList[state.currentDrinkIndex]["englishName"],
+                    menuKoreanName: state.drinkList[state.currentDrinkIndex]["koreanName"],
+                    menuDescription1: state.drinkList[state.currentDrinkIndex]["description1"],
+                    menuDescription2: state.drinkList[state.currentDrinkIndex]["description2"],
+                    menuDescription3: state.drinkList[state.currentDrinkIndex]["description3"],
+                    menuDescription4: state.drinkList[state.currentDrinkIndex]["description4"],
+                }
             }
+            
         case INIT_MENU_DATA:
             return {
                 ...state,
-                menuEnglishName: state.menuList[state.currentMenuIndex]["englishName"],
-                menuKoreanName: state.menuList[state.currentMenuIndex]["koreanName"],
-                menuDescription1: state.menuList[state.currentMenuIndex]["description1"],
-                menuDescription2: state.menuList[state.currentMenuIndex]["description2"],
-                menuDescription3: state.menuList[state.currentMenuIndex]["description3"],
-                menuDescription4: state.menuList[state.currentMenuIndex]["description4"],
+                menuEnglishName: state.dessertList[state.currentDessertIndex]["englishName"],
+                menuKoreanName: state.dessertList[state.currentDessertIndex]["koreanName"],
+                menuDescription1: state.dessertList[state.currentDessertIndex]["description1"],
+                menuDescription2: state.dessertList[state.currentDessertIndex]["description2"],
+                menuDescription3: state.dessertList[state.currentDessertIndex]["description3"],
+                menuDescription4: state.dessertList[state.currentDessertIndex]["description4"],
             }
         case SWIPE_MENU_TO_LEFT:
             let leftMenuIndex;
-            if (state.currentMenuIndex === 0) {
-                leftMenuIndex = state.menuList.length - 1;
+            if (state.menuType === '디저트') {
+                if (state.currentDessertIndex === 0) {
+                    leftMenuIndex = state.dessertList.length - 1;
+                } else {
+                    leftMenuIndex = state.currentDessertIndex - 1;
+                }
+                return {
+                    ...state,
+                    currentDessertIndex: leftMenuIndex,
+                    menuEnglishName: state.dessertList[leftMenuIndex]["englishName"],
+                    menuKoreanName: state.dessertList[leftMenuIndex]["koreanName"],
+                    menuDescription1: state.dessertList[leftMenuIndex]["description1"],
+                    menuDescription2: state.dessertList[leftMenuIndex]["description2"],
+                    menuDescription3: state.dessertList[leftMenuIndex]["description3"],
+                    menuDescription4: state.dessertList[leftMenuIndex]["description4"],
+                }
             } else {
-                leftMenuIndex = state.currentMenuIndex - 1;
-            }
-            return {
-                ...state,
-                currentMenuIndex: leftMenuIndex,
-                menuEnglishName: state.menuList[leftMenuIndex]["englishName"],
-                menuKoreanName: state.menuList[leftMenuIndex]["koreanName"],
-                menuDescription1: state.menuList[leftMenuIndex]["description1"],
-                menuDescription2: state.menuList[leftMenuIndex]["description2"],
-                menuDescription3: state.menuList[leftMenuIndex]["description3"],
-                menuDescription4: state.menuList[leftMenuIndex]["description4"],
+                if (state.currentDrinkIndex === 0) {
+                    leftMenuIndex = state.drinkList.length - 1;
+                } else {
+                    leftMenuIndex = state.currentDrinkIndex - 1;
+                }
+                return {
+                    ...state,
+                    currentMenuIndex: leftMenuIndex,
+                    menuEnglishName: state.drinkList[leftMenuIndex]["englishName"],
+                    menuKoreanName: state.drinkList[leftMenuIndex]["koreanName"],
+                    menuDescription1: state.drinkList[leftMenuIndex]["description1"],
+                    menuDescription2: state.drinkList[leftMenuIndex]["description2"],
+                    menuDescription3: state.drinkList[leftMenuIndex]["description3"],
+                    menuDescription4: state.drinkList[leftMenuIndex]["description4"],
+                }
             }
         case SWIPE_MENU_TO_RIGHT:
             let rightMenuIndex;
-            if (state.currentMenuIndex === (state.menuList.length-1)) {
-                rightMenuIndex = 0;
+            if (state.menuType === '디저트') {
+                if (state.currentDessertIndex === (state.dessertList.length-1)) {
+                    rightMenuIndex = 0;
+                } else {
+                    rightMenuIndex = state.currentDessertIndex + 1;
+                }
+                return {
+                    ...state,
+                    currentDessertIndex: rightMenuIndex,
+                    menuEnglishName: state.dessertList[rightMenuIndex]["englishName"],
+                    menuKoreanName: state.dessertList[rightMenuIndex]["koreanName"],
+                    menuDescription1: state.dessertList[rightMenuIndex]["description1"],
+                    menuDescription2: state.dessertList[rightMenuIndex]["description2"],
+                    menuDescription3: state.dessertList[rightMenuIndex]["description3"],
+                    menuDescription4: state.dessertList[rightMenuIndex]["description4"],
+                }
             } else {
-                rightMenuIndex = state.currentMenuIndex + 1;
-            }
-            return {
-                ...state,
-                currentMenuIndex: rightMenuIndex,
-                menuEnglishName: state.menuList[rightMenuIndex]["englishName"],
-                menuKoreanName: state.menuList[rightMenuIndex]["koreanName"],
-                menuDescription1: state.menuList[rightMenuIndex]["description1"],
-                menuDescription2: state.menuList[rightMenuIndex]["description2"],
-                menuDescription3: state.menuList[rightMenuIndex]["description3"],
-                menuDescription4: state.menuList[rightMenuIndex]["description4"],
+                if (state.currentDrinkIndex === (state.dessertList.length-1)) {
+                    rightMenuIndex = 0;
+                } else {
+                    rightMenuIndex = state.currentDrinkIndex + 1;
+                }
+                return {
+                    ...state,
+                    currentDrinkIndex: rightMenuIndex,
+                    menuEnglishName: state.drinkList[rightMenuIndex]["englishName"],
+                    menuKoreanName: state.drinkList[rightMenuIndex]["koreanName"],
+                    menuDescription1: state.drinkList[rightMenuIndex]["description1"],
+                    menuDescription2: state.drinkList[rightMenuIndex]["description2"],
+                    menuDescription3: state.drinkList[rightMenuIndex]["description3"],
+                    menuDescription4: state.drinkList[rightMenuIndex]["description4"],
+                }
             }
         default:
             return state;
