db371c9d0cd5e80bdcb520a2f90825c07f9c7bd2
Gyubin <gbcompany27@gmail.com>
2022-10-17 22:23:07 +0900

    add MenuBlock to personal page

--- src/components/views/Personal/Personal.js
+++ src/components/views/Personal/Personal.js
@@ -3,12 +3,14 @@ import './Personal.css';
 import Header from '../../library/ui/header/Header/Header';
 import Footer from '../../library/ui/footer/Footer/Footer';
 import Banner from '../../library/ui/unit/Banner/Banner';
+import MenuBlock from '../../library/ui/unit/MenuBlock/MenuBlock';
 
 export default function Personal() {
     return (
         <div className='page'>
             <Header></Header>
             <Banner></Banner>
+            <MenuBlock></MenuBlock>
             <Footer></Footer>
         </div>
     )
