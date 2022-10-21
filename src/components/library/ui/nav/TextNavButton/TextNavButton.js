5e33cd50e81e904aa649a57ffec5278dc631e89a
gyubin <gbcompany27@gmail.com>
2022-10-21 21:10:12 +0900

    add hover color to nav text button

--- src/components/library/ui/nav/TextNavButton/TextNavButton.js
+++ src/components/library/ui/nav/TextNavButton/TextNavButton.js
@@ -5,12 +5,12 @@ export default function TextNavButton(props) {
     if (props.isOn) {
         NavMenuText = 
             <button className='nav-menu'>
-                <span className='nav-menu-text' style={{color:'#EA5B30'}}>{props.text}</span>
+                <span className='nav-menu-text on'>{props.text}</span>
             </button>
     } else {
         NavMenuText = 
             <button className='nav-menu' onClick={() => props.navEvent(props.pageName)}>
-                <span className='nav-menu-text' style={{color:'#CACACA'}}>{props.text}</span>
+                <span className='nav-menu-text off'>{props.text}</span>
             </button>
     }
     return (    
