f0c274e6464511edb639a8e8e52d3c4680145405
gyubin <gbcompany27@gmail.com>
2022-10-21 22:25:11 +0900

    add order item's name, quantity, and total price to order sheet

--- src/components/library/ui/unit/OrderSheet/OrderSheet.js
+++ src/components/library/ui/unit/OrderSheet/OrderSheet.js
@@ -39,12 +39,24 @@ export default function OrderSheet(props) {
         totalPrice = 0;
     }
 
+    const OrderItem = props.quantityArray.map((quantity,index) => (
+        <OrderSheetItem
+            quantity = {quantity}
+            price = {props.priceArray[index]}
+            name = {props.nameArray[index]}
+            key={'order_item_'+props.nameArray[index]}></OrderSheetItem>
+    ))
+
     return (
         <div className='order-sheet-container'>
             <h3 className='order-sheet-title'>장바구니</h3>
-            <div style={{'flex':'1'}}></div>
+            <div style={{'flex':'1','minHeight':'20px'}}></div>
+            <div className='order-sheet-items-container'>
+                {OrderItem}
+            </div>
+            <div style={{'minHeight':'20px'}}></div>
             <div className='order-sheet-divider'></div>
-            <div style={{'minHeight':'10px'}}></div>
+            <div style={{'minHeight':'20px'}}></div>
             <div className='order-sheet-total-price-container'>
                 <span className='order-sheet-total-price'>
                     총 {totalPrice.toLocaleString()}원
@@ -56,4 +68,22 @@ export default function OrderSheet(props) {
             </div>
         </div>
     )
+}
+
+function OrderSheetItem(props) {
+    let itemTotalPrice
+    if (props.quantity && props.price) {
+        itemTotalPrice = props.quantity * props.price;
+    } else {
+        itemTotalPrice = 0;
+    }
+    if (itemTotalPrice > 0) {
+        return (
+            <div className='order-sheet-item-container'>
+                <span className='order-sheet-item-text'>{props.name} x {props.quantity}</span>
+                <div style={{'flex':'1'}}></div>
+                <span className='order-sheet-item-text'>{itemTotalPrice.toLocaleString()}원</span>
+            </div>
+        )
+    } 
 }
