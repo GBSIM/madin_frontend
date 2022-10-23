c8c304c42157c2ce11fe816d992f925ac329f4a2
gyubin <gbcompany27@gmail.com>
2022-10-23 17:38:46 +0900

    add test code

--- src/components/library/function/KakaoLogin.js
+++ src/components/library/function/KakaoLogin.js
@@ -17,17 +17,18 @@ export const KakaoRedirectHandler = async() => {
   const dispatch = useDispatch();
   let params = new URL(document.location.toString()).searchParams;
   let code = params.get("code");
-  const kakaoLoginResponse = await axios.post('http://localhost:3000/user/kakaologin',
-    {
-      "code": code,
-      "redirectUri": process.env.REACT_APP_REDIRECT_URL
-    }
-  )
-  const user = kakaoLoginResponse.data.user;
-  console.log(user);
+  console.log(code);
+  // const kakaoLoginResponse = await axios.post('https://api.madinbakery.com/user/kakaologin',
+  //   {
+  //     "code": code,
+  //     "redirectUri": process.env.REACT_APP_REDIRECT_URL
+  //   }
+  // )
+  // const user = kakaoLoginResponse.data.user;
+  // console.log(user);
   window.history.replaceState({}, null, window.location.pathname);
-  dispatch(saveUserInfo(user));
-  setCookie("token",user["token"],1);
-  setCookie("email",user["email"],1);
-  setCookie("socialId",user["socialId"],1);
+  // dispatch(saveUserInfo(user));
+  // setCookie("token",user["token"],1);
+  // setCookie("email",user["email"],1);
+  // setCookie("socialId",user["socialId"],1);
 };
