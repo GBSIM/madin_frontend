257f35a6a38a9505e6e213b04cf9f4c2e67916b9
gyubin <gbcompany27@gmail.com>
2022-10-23 11:13:38 +0900

    link the kakaologin between frontend and backend

--- src/components/library/ui/account/KakaoLogin/KakaoLogin.js
+++ src/components/library/ui/account/KakaoLogin/KakaoLogin.js
@@ -15,13 +15,13 @@ export const loginWithKakao = () =>{
 export const KakaoRedirectHandler = async() => {
   let params = new URL(document.location.toString()).searchParams;
   let code = params.get("code");
-  console.log(code);
-  let grant_type = "authorization_code";
-  let client_id = "c49e9d7fad13c64229c3899523a2ba6b";
-  // await axios.post('https://api.madinbakery.com/user/kakao',
-  //   {
-  //     "code": code,
-  //     "redirectUri": process.env.REACT_APP_REDIRECT_URL
-  //   }
-  // )
+  const kakaoLoginResponse = await axios.post('https://api.madinbakery.com/user/kakao',
+    {
+      "code": code,
+      "redirectUri": process.env.REACT_APP_REDIRECT_URL
+    }
+  )
+  const user = kakaoLoginResponse.data.user;
+  console.log(user);
+  window.history.replaceState({}, null, window.location.pathname);
 };
