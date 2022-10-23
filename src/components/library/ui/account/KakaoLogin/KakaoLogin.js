679be0aa638a190ba10211a2fcfb3ba905c0474d
gyubin <gbcompany27@gmail.com>
2022-10-23 11:50:13 +0900

    activate kakao login

--- src/components/library/ui/account/KakaoLogin/KakaoLogin.js
+++ src/components/library/ui/account/KakaoLogin/KakaoLogin.js
@@ -16,13 +16,13 @@ export const KakaoRedirectHandler = async() => {
   let params = new URL(document.location.toString()).searchParams;
   let code = params.get("code");
   console.log(code);
-  // const kakaoLoginResponse = await axios.post('https://api.madinbakery.com/user/kakao',
-  //   {
-  //     "code": code,
-  //     "redirectUri": process.env.REACT_APP_REDIRECT_URL
-  //   }
-  // )
-  // const user = kakaoLoginResponse.data.user;
-  // console.log(user);
+  const kakaoLoginResponse = await axios.post('https://api.madinbakery.com/user/kakao',
+    {
+      "code": code,
+      "redirectUri": process.env.REACT_APP_REDIRECT_URL
+    }
+  )
+  const user = kakaoLoginResponse.data.user;
+  console.log(user);
   window.history.replaceState({}, null, window.location.pathname);
 };
