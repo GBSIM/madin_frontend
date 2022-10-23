0be6f927d7dbd4b902e3c0565c36454eb7916707
gyubin <gbcompany27@gmail.com>
2022-10-23 15:45:21 +0900

    add login button removal function when login

--- src/components/library/ui/account/LoginButton/LoginButton.js
+++ src/components/library/ui/account/LoginButton/LoginButton.js
@@ -1,14 +1,19 @@
 import OrangeButton from "../../unit/OrangeButton/OrangeButton";
 
+import { useSelector } from "react-redux";
 import { loginWithKakao } from "../KakaoLogin/KakaoLogin";
 
 export default function LoginButton() {
-    return (
-        <OrangeButton
-            width = '85px'
-            height = '33px'
-            borderRadius = '6px'
-            text='로그인'
-            clickEvent={loginWithKakao}></OrangeButton>
-    )
+    const { isLogin } = useSelector(state => state.user);
+
+    if (!isLogin) {
+        return (
+            <OrangeButton
+                width = '85px'
+                height = '33px'
+                borderRadius = '6px'
+                text='로그인'
+                clickEvent={loginWithKakao}></OrangeButton>
+        )
+    }
 }
