f68ef4f8cb31a0b0be4c1ce9f79b0c634d008780
gyubin <gbcompany27@gmail.com>
2022-10-24 17:10:43 +0900

    remove test codes'

--- src/components/library/ui/account/LoginButton/LoginButton.js
+++ src/components/library/ui/account/LoginButton/LoginButton.js
@@ -1,7 +1,9 @@
 import OrangeButton from "../../unit/OrangeButton/OrangeButton";
 
 import { useSelector } from "react-redux";
+
 import { loginWithKakao } from "../../../function/KakaoLogin";
+import { UserAuth } from "../../../function/Auth";
 
 export default function LoginButton() {
     const { isLogin } = useSelector(state => state.user);
