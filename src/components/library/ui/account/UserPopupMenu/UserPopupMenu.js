64b98e51d44741bd59679618550445c4c9285c2a
Gyubin <gbcompany27@gmail.com>
2022-10-25 23:11:55 +0900

    add menus to popup user menu

--- src/components/library/ui/account/UserPopupMenu/UserPopupMenu.js
+++ src/components/library/ui/account/UserPopupMenu/UserPopupMenu.js
@@ -8,7 +8,22 @@ export default function UserPopupMenu() {
     if (userMenuPopup) {
         return (
             <div className='user-popup-menu-container'>
+                <UserMenu text='주문내역'></UserMenu>
+                <UserMenu text='배송지 관리'></UserMenu>
+                <UserMenu text='적립금'></UserMenu>
+                <UserMenu text='쿠폰'></UserMenu>
+                <UserMenu text='로그아웃'></UserMenu>
             </div>
         )
     }
+}
+
+function UserMenu(props) {
+    return (
+        <div className='user-menu-container'>
+            <button className='user-menu-button' onClick={() => props.clickEvent}>
+                <span className='user-menu-button-text'>{props.text}</span>
+            </button>
+        </div>
+    )
 }
