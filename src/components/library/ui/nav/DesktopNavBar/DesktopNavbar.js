a2231cf1ba394065754f04580d6523c079cd3c0c
Gyubin <gbcompany27@gmail.com>
2022-10-27 00:40:12 +0900

    update the nav bar coloring policy

--- src/components/library/ui/nav/DesktopNavBar/DesktopNavbar.js
+++ src/components/library/ui/nav/DesktopNavBar/DesktopNavbar.js
@@ -18,11 +18,11 @@ export default function Navbar() {
 
     return (
         <div className='nav-bar'>
-            <TextNavButton text='개인구매' isOn={page==='personal'} navEvent={navPageEvent} pageName={'personal'}></TextNavButton>
+            <TextNavButton text='개인구매' isOn={page==='personal' || page==='personalorder'} navEvent={navPageEvent} pageName={'personal'}></TextNavButton>
             <div className='nav-bar-divider'></div>
-            <TextNavButton text='답례품' isOn={page==='group'} navEvent={navPageEvent} pageName={'group'}></TextNavButton>
+            <TextNavButton text='답례품' isOn={page==='group' || page==='grouporder'} navEvent={navPageEvent} pageName={'group'}></TextNavButton>
             <div className='nav-bar-divider'></div>
-            <TextNavButton text='픽업예약' isOn={page==='pickup'} navEvent={navPageEvent} pageName={'pickup'}></TextNavButton>
+            <TextNavButton text='픽업예약' isOn={page==='pickup' || page==='pickuporder'} navEvent={navPageEvent} pageName={'pickup'}></TextNavButton>
         </div>
     )
 }
