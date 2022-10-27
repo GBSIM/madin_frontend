9032d6758d5166201998d9650986566846f423fd
Gyubin <gbcompany27@gmail.com>
2022-10-27 23:21:21 +0900

    add user auth to personal order

--- src/components/views/PersonalOrder/PersonalOrder.js
+++ src/components/views/PersonalOrder/PersonalOrder.js
@@ -1,17 +1,39 @@
 import './PersonalOrder.css';
 
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+
+import { changePage } from '../../../_reducers/nav';
+import { UserAuth } from '../../library/function/Auth';
+
 import Header from '../../library/ui/header/Header/Header';
 import Footer from '../../library/ui/footer/Footer/Footer';
 import PersonalOrderMenus from '../../library/ui/personalorder/PersonalOrderMenus/PersonalOrderMenus';
+import { useEffect } from 'react';
 
 export default function PersonalOrder() {
-    return (
-        <div className='page'>
-            <Header></Header>
-            <div className='order-contents-container'>
-                <PersonalOrderMenus></PersonalOrderMenus>
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { isLogin } = useSelector(state => state.user);
+
+    UserAuth();
+
+    useEffect(() => {
+        if (!isLogin) {
+            dispatch(changePage('order'));
+            navigate('/personal');
+        }
+    }, [])
+    
+    if (isLogin) {
+        return (
+            <div className='page'>
+                <Header></Header>
+                <div className='order-contents-container'>
+                    <PersonalOrderMenus></PersonalOrderMenus>
+                </div>
+                <Footer></Footer>
             </div>
-            <Footer></Footer>
-        </div>
-    )
+        )
+    }    
 }
