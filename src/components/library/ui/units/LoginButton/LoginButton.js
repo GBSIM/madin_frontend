3b9e1195844516c332f5c859c3bd5d5cef5836fc
Gyubin <gbcompany27@gmail.com>
2022-11-18 00:15:19 +0900

    add social login button design

--- src/components/library/ui/units/LoginButton/LoginButton.js
+++ src/components/library/ui/units/LoginButton/LoginButton.js
@@ -39,16 +39,40 @@ function SocialLoginBox(props) {
                         </button>
                     </div>
                     <img src={require('../../../images/logo_text.png')} className='social-login-box-logo-image'></img>
-                    <div style={{'minHeight':'60px'}}></div>
+                    <div style={{'minHeight':'30px'}}></div>
                     <span className='social-login-guide'>아래의 버튼을 눌러 간편 로그인해주세요.</span>
                     <div style={{'minHeight':'30px'}}></div>
                     <div className='social-login-buttons-container'>
-                        <button className='social-login-button'>
-                            <img src={require('../../../images/kakao_login_medium_wide.png')} className='social-login-button-image'></img>
-                        </button>
+                        <SocialLoginButton
+                            backgroundColor='#FEE500'
+                            borderColor='#FEE500'
+                            textColor='#141414'
+                            text='카카오 로그인'
+                            img={require('../../../icons/kakao.png')}/>
+                        <SocialLoginButton
+                            backgroundColor='#03C75A'
+                            borderColor='#03C75A'
+                            textColor='#fff'
+                            text='네이버 로그인'
+                            img={require('../../../icons/naver_white.png')}/>
+                        <SocialLoginButton
+                            backgroundColor='#fff'
+                            borderColor='#eee'
+                            textColor='#141414'
+                            text='구글 로그인'
+                            img={require('../../../icons/google.png')}/>
                     </div>
                 </div>
             </div>
         )
     }   
+}
+
+function SocialLoginButton(props) {
+    return (
+        <button className='social-login-button' style={{'background':props.backgroundColor,'borderColor':props.borderColor}}>
+            <img src={props.img} className='social-login-button-image'></img>
+            <span className='social-login-button-text' style={{'color':props.textColor}}>{props.text}</span>
+        </button>
+    )
 }
