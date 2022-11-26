b24cef691bc0a4e53f04d7ec8de32015cbfbb554
gyubin <gbcompany27@gmail.com>
2022-11-26 18:46:22 +0900

    add cart menu delete window

--- src/components/library/ui/units/Cart/Cart.js
+++ src/components/library/ui/units/Cart/Cart.js
@@ -1,8 +1,10 @@
 import './Cart.css';
 import './CartMenu.css';
+import './DeleteWindow.css';
 
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 import { getCookie } from '../Cookie/Cookie';
 import { saveCart } from '../../../../../_reducers/user';
@@ -63,6 +65,8 @@ export default function Cart(props) {
 
 function CartMenu(props) {
     const dispath = useDispatch();
+    const [isDeleteWindowOn, setDeleteWindowOn] = useState(false);
+
     const addQuantity = async() => {
         const token = getCookie('token');
         if (token) {
@@ -91,22 +95,12 @@ function CartMenu(props) {
         }
     }
 
-    const deleteMenu = async() => {
-        const token = getCookie('token');
-        if (token) {
-            await axios.delete('https://api.madinbakery.com/user/cart',{
-                data: {
-                    "token": token,
-                    "menuId": props.id
-                }
-            }).then((res) => {
-                console.log(res);
-                const user = res.data.user;
-                dispath(saveCart(user["cart"]));
-            }).catch((err) => {
-                console.log(err);
-            });
-        }
+    const openDeleteWindow = () => {
+        setDeleteWindowOn(true);
+    }
+
+    const closeDeleteWindow = () => {
+        setDeleteWindowOn(false);
     }
 
     let CheckButton;
@@ -155,9 +149,53 @@ function CartMenu(props) {
                     {(props.price*props.quantity).toLocaleString()}원
                 </h3>
             </div>
-            <button className='cart-menu-delete-button' onClick={() => deleteMenu()}>
+            <button className='cart-menu-delete-button' onClick={() => openDeleteWindow()}>
                 <span className='cart-menu-delete-button-text'>삭제하기</span>
             </button>
+            <DeleteWindow 
+                isOn={isDeleteWindowOn} 
+                closeEvent={closeDeleteWindow}
+                id={props.id}></DeleteWindow>
         </div>
     )
+}
+
+function DeleteWindow(props) {
+    const dispath = useDispatch();
+    const deleteMenu = async() => {
+        const token = getCookie('token');
+        if (token) {
+            await axios.delete('https://api.madinbakery.com/user/cart',{
+                data: {
+                    "token": token,
+                    "menuId": props.id
+                }
+            }).then((res) => {
+                console.log(res);
+                const user = res.data.user;
+                dispath(saveCart(user["cart"]));
+                props.closeEvent();
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    }
+
+    if (props.isOn) {
+        return (
+            <div className='delete-window-container'>
+                <div className='delete-window'>
+                    <span className='delete-window-guide'>장바구니에서 삭제하시겠어요?</span>
+                    <div style={{'minHeight':'60px'}}></div>
+                    <button className='delete-window-delete-button' onClick={() => deleteMenu()}>
+                        <span className='delete-window-delete-button-text'>삭제하기</span>
+                    </button>
+                    <div style={{'minHeight':'10px'}}></div>
+                    <button className='delete-window-cancel-button' onClick={() => {props.closeEvent()}}>
+                        <span className='delete-window-cancel-button-text'>닫기</span>
+                    </button>
+                </div>
+            </div>
+        )        
+    }
 }
