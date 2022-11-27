e92155770c577a8c5c68be3d620d5cbb1f399006
gyubin <gbcompany27@gmail.com>
2022-11-27 18:21:20 +0900

    implement add shipping function

--- src/components/library/ui/units/Shipping/Shipping.js
+++ src/components/library/ui/units/Shipping/Shipping.js
@@ -6,6 +6,11 @@ import './ShippingAddWindow.css';
 import DaumPostcode from 'react-daum-postcode';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
+
+import { getCookie } from '../Cookie/Cookie';
+import { authUser } from '../LoginButton/KakaoLogin';
+import { saveShipping } from '../../../../../_reducers/user';
 
 export default function Shipping(props) {
     const [isShippingAddWindowOn, setShippingAddWindowOn] = useState(false);
@@ -19,13 +24,14 @@ export default function Shipping(props) {
     }
 
     let ShippingInfos;
-    if (props.shippings.length > 0) {
-        ShippingInfos = props.shippings.map((shipping) => (
-            <Shipping
+    if (props.shippings) {
+        ShippingInfos = props.shippings.map((shipping,index) => (
+            <ShippingInfo
                 name={shipping["tag"]}
-                address={shipping["basicAderess"]+shipping["detailAddress"]}
+                address={shipping["basicAddress"]+' '+shipping["detailAddress"]}
                 phone={shipping["phone"]}
-                receiver={shipping["name"]}></Shipping>
+                receiver={shipping["name"]}
+                key={'shipping_'+String(index)}></ShippingInfo>
         ))
     } else {
         ShippingInfos = 
@@ -78,7 +84,7 @@ function ShippingInfo(props) {
 function ShippingAddButton(props) {
     return (
         <button className='shipping-add-button' onClick={() => props.clickEvent()}>
-            <img className='shipping-add-button-image' src={require('../../../icons/plus_grey.png')}></img>
+            <img className='shipping-add-button-image' src={require('../../../icons/plus_grey.png')} alt='add'></img>
             <span className='shipping-add-button-guide'>새로운 주소 추가하기</span>
         </button>
     )
@@ -90,14 +96,43 @@ function ShippingAddWindow(props) {
     const [isDetailAddressSelected, setDetailAddressSelected] = useState(false);
     const [basicAddress, setBasicAddress] = useState(null);
     const [detailAddress, setDetailAddress] = useState(null);
+    const [name, setName] = useState(null);
+    const [phone, setPhone] = useState(null);
 
     const updateDetailAddress = (e) => {
         dispatch(setDetailAddress(e.target.value));
+    }
+
+    const updateName = (e) => {
+        dispatch(setName(e.target.value));
+    }
+
+    const updatePhone = (e) => {
+        dispatch(setPhone(e.target.value));
     }
     
     const completeSetBasicAddress = (data) => {
         setBasicAddressSelected(true);
         setBasicAddress(data.address);
+    }
+
+    const completeSetDetailAddress = () => {
+        setDetailAddressSelected(true);
+    }
+
+    const completeAddAddress = async() => {
+        const token = getCookie('token');
+        await axios.post('https://api.madinbakery.com/shipping', {
+            "name": name,
+            "phone": phone,
+            "basicAddress": basicAddress,
+            "detailAddress": detailAddress,
+            "token": token,
+            "tag": "배송지",
+        });
+        const user = await authUser();
+        dispatch(saveShipping(user["shippings"]));
+        props.closeEvent();
     }
 
     let DaumAddressAPI;
@@ -119,8 +154,37 @@ function ShippingAddWindow(props) {
                     </div>
                 </div>
                 <div style={{'minHeight':'50px'}}></div>
-                <button className='shipping-add-window-save-button'>
+                <button className='shipping-add-window-save-button' onClick={() => completeSetDetailAddress()}>
                     <span className='shipping-add-window-save-button-text'>다음</span>
+                </button>
+            </div>
+    }
+
+    let ReceiverInput;
+    if (isDetailAddressSelected) {
+        ReceiverInput = 
+            <div className='shipping-add-window-detail-address-input-container'>
+                <div className='shipping-add-window-basic-address-frame'>
+                    <span className='shipping-add-window-basic-address'>{basicAddress}</span>
+                </div>
+                <div className='shipping-add-window-basic-address-frame'>
+                    <span className='shipping-add-window-basic-address'>{detailAddress}</span>
+                </div>
+                <div className='shipping-add-window-detail-address-input-row'>
+                    <span className='shipping-add-window-detail-address-title'>받는 사람</span>
+                    <div className='shipping-add-window-detail-address-input-frame'>
+                        <input className='shipping-add-window-detail-address' value={name} onChange={updateName}></input>
+                    </div>
+                </div>
+                <div className='shipping-add-window-detail-address-input-row'>
+                    <span className='shipping-add-window-detail-address-title'>연락처</span>
+                    <div className='shipping-add-window-detail-address-input-frame'>
+                        <input className='shipping-add-window-detail-address' value={phone} onChange={updatePhone}></input>
+                    </div>
+                </div>
+                <div style={{'minHeight':'30px'}}></div>
+                <button className='shipping-add-window-save-button' onClick={() => completeAddAddress()}>
+                    <span className='shipping-add-window-save-button-text'>완료</span>
                 </button>
             </div>
     }
@@ -131,13 +195,13 @@ function ShippingAddWindow(props) {
                 <div className='shipping-add-window'>
                     {DaumAddressAPI}
                     {DetailAddressInput}
+                    {ReceiverInput}
                     <div style={{'minHeight':'10px'}}></div>
                     <button className='shipping-add-window-cancel-button' onClick={() => {props.closeEvent()}}>
                         <span className='shipping-add-window-cancel-button-text'>닫기</span>
                     </button>
                 </div>
             </div>
-            
         )
     }
 }
